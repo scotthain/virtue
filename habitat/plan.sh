c00b6bb671f7c782b7f40efd3bf0377b9b61e226
Scott Hain <shain@chef.io>
2017-08-16 16:52:58 -0700

    Fox Pause
    Signed-off-by: Scott Hain <shain@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -8,4 +8,13 @@ pkg_svc_group=$pkg_svc_user
 
 #pkg_scaffolding=core/scaffolding-node
 # this is here because https://github.com/habitat-sh/core-plans/pull/692
-pkg_scaffolding=scotthain/scaffolding-node/0.6.0/20170804214125
+#pkg_scaffolding=scotthain/scaffolding-node/0.6.0/20170804214125
+
+do_install() {
+  return 0
+}
+
+do_build() {
+  return 0   
+}
+
